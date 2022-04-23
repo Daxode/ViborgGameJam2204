5aac694a61809f692decf2e34724a2e8d8351d6a
Daxode <dax@daxode.dk>
2022-04-23 16:55:25 +0200

    Trying to make bullets work

---
+++ ViborgGameJam/Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,50 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using UnityEngine;
+
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+public partial class DamageSystem : SystemBase {
+    private StepPhysicsWorld physicsWorld;
+    private EndFixedStepSimulationEntityCommandBufferSystem _entityCommandBufferSystem;
+    protected override void OnCreate() {
+        physicsWorld = World.GetExistingSystem<StepPhysicsWorld>();
+        _entityCommandBufferSystem = World.GetExistingSystem<EndFixedStepSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate() {
+        var job = new BulletTriggerJob {
+            bullets = GetComponentDataFromEntity<BulletDamage>(true),
+            healths = GetComponentDataFromEntity<Health>(),
+            ecb = _entityCommandBufferSystem.CreateCommandBuffer()
+        };
+        Dependency = job.Schedule(physicsWorld.Simulation, Dependency);
+        _entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
+
+//[BurstCompile]
+public struct BulletTriggerJob : ITriggerEventsJob {
+    [ReadOnly] public ComponentDataFromEntity<BulletDamage> bullets;
+    public ComponentDataFromEntity<Health> healths;
+    public EntityCommandBuffer ecb;
+
+    public void Execute(TriggerEvent triggerEvent) {
+        var bulletEntity = triggerEvent.EntityB;
+        var healthEntity = triggerEvent.EntityA;
+
+        if (!bullets.HasComponent(bulletEntity)) return;
+        
+        if (healths.HasComponent(healthEntity)) {
+            var health = healths[healthEntity];
+            health.Left--;
+            if (health.Left < 0) {
+                ecb.DestroyEntity(healthEntity);
+            }
+        }
+            
+        ecb.DestroyEntity(bulletEntity);
+    }
+}
--- ViborgGameJam/Assets/Scripts/PlayerAuthor.cs
+++ ViborgGameJam/Assets/Scripts/PlayerAuthor.cs
@@ -2,7 +2,14 @@
 using UnityEngine;
 
 public class PlayerAuthor : MonoBehaviour, IConvertGameObjectToEntity {
+    public int health = 3;
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
         dstManager.AddComponentData(entity, new PlayerTag());
+        dstManager.AddComponentData(entity, new Health { Left = health, Total = health });
     }
+}
+
+public struct Health : IComponentData {
+    public int Left;
+    public int Total;
 }
